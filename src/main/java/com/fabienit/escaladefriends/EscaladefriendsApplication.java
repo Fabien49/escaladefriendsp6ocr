c50d8548fb717ca676223ea11ef057c04416ed5c
Fabien49 <fabien.chapeau@gmail.com>
2020-04-06 12:24:00 +0200

    squelette architecture spring boot

---
+++ src/main/java/com/fabienit/escaladefriends/EscaladefriendsApplication.java
@@ -0,0 +1,13 @@
+package com.fabienit.escaladefriends;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EscaladefriendsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EscaladefriendsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/fabienit/escaladefriends/EscaladefriendsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fabienit.escaladefriends;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EscaladefriendsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
